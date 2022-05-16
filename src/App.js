d4a5a67d0f9fed3b7a3c7eede988b7f3f5ae074b
BackbenchCoder-py <80917802+BackbenchCoder-py@users.noreply.github.com>
2022-05-16 23:44:46 +0530

    Navbar made with reactJs and pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Logo from './components/Logo'
+// import LogoStyle from './components/Logo';
+import Links from './components/Links'
+import Button from './components/Button'
 import './App.css';
 
 function App() {
+  let nameOfLogo = 'LOGOBAKERY';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navbar'>
+        <Logo name = {nameOfLogo} />
+        <Links />
+        <Button />
+      </nav>
     </div>
   );
 }
---
+++ src/components/Links.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './LinksStyle.css';
+
+const Links = () => {
+    return(
+        <div className="links">
+            <div>{"Services"}</div>
+            <div>{"Projects"}</div>
+            <div>{"About"}</div>
+        </div>
+    )
+}
+
+export default Links;
