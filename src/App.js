1a456c7c4e676e52751279a52d7e0700b061a4cf
BackbenchCoder-py <80917802+BackbenchCoder-py@users.noreply.github.com>
2022-05-17 00:05:30 +0530

    Navbar made with React and pushed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import Logo from './components/Logo'
+import Logo from "./components/Logo";
 // import LogoStyle from './components/Logo';
-import Links from './components/Links'
-import Button from './components/Button'
-import './App.css';
+import Links from "./components/Links";
+import Button from "./components/Button";
+import "./App.css";
 
 function App() {
-  let nameOfLogo = 'LOGOBAKERY';
+  let nameOfLogo = "LOGOBAKERY";
+  let data = ["Services", "Projects", "About"];
   return (
     <div className="App">
-      <nav className='navbar'>
-        <Logo name = {nameOfLogo} />
-        <Links />
+      <nav className="navbar">
+        <Logo name={nameOfLogo} />
+        <div className="links">
+          <Links links={data} />
+        </div>
         <Button />
       </nav>
     </div>
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import './LinksStyle.css';
 
-const Links = () => {
+const Links = ({links}) => {
+    const list = links.map((ele) => {
+        console.log(ele);
+        return(
+            <>
+                <li>{ele}</li>
+            </>
+        )
+    })
     return(
-        <div className="links">
-            <div>{"Services"}</div>
-            <div>{"Projects"}</div>
-            <div>{"About"}</div>
-        </div>
+        <ul className="x">
+            {list}
+        </ul>
     )
+    // return(
+    //     <div className="links">
+    //         <div>{"Services"}</div>
+    //         <div>{"Projects"}</div>
+    //         <div>{"About"}</div>
+    //     </div>
+    // )
 }
 
 export default Links;
